0951eaa372298a1ed825808f1fafd7eb2b4ca056
hugo <hugoandregg@gmail.com>
2014-10-19 03:53:31 -0300

    slide funcionando

--- core/views.py
+++ core/views.py
@@ -5,8 +5,9 @@ import os
 def home(request):
 	pdf = File.objects.all()[0]
 	print str(pdf.file)[7:]
-	your_command = 'cd ;cd projetos/slides/slides/media/photos;cp %s teste;cd teste;convert %s *.jpg; rm %s' % (str(pdf.file)[7:], str(pdf.file)[7:], str(pdf.file)[7:])
-	os.system(your_command)
-	context = {}
+	command = 'cd ;cd projetos/slides/converter/media/photos;mkdir %s;cp %s teste;cd teste;convert %s *.jpg; rm %s' % ( (str(pdf.file)[7:-4] + 'dir'), str(pdf.file)[7:], str(pdf.file)[7:], str(pdf.file)[7:])
+	#os.system(command)
+	print command
+	context = {'range': range(41)}
 	template = "home.html"
 	return render(request, template, context)
