c3c61594b6ce5e804740611d1dba5d98f2e2202f
hugo <hugoandregg@gmail.com>
2014-10-19 18:23:37 -0300

    slides completos

--- core/views.py
+++ core/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from .models import File
-import os
+import os, subprocess
 
 def home(request):
 	pdf = File.objects.all()[0]
 	print str(pdf.file)[7:]
-	command = 'cd ;cd projetos/slides/converter/media/photos;mkdir %s;cp %s teste;cd teste;convert %s *.jpg; rm %s' % ( (str(pdf.file)[7:-4] + 'dir'), str(pdf.file)[7:], str(pdf.file)[7:], str(pdf.file)[7:])
-	#os.system(command)
-	print command
-	context = {'range': range(41)}
+	#ainda falta criar pasta para cada slide
+	comando = 'cd ;cd projetos/slides/converter/media/photos;cp %s ../../templates/assets/img;cd ../../templates/assets/img;convert %s *.jpg; rm %s' % (str(pdf.file)[7:], str(pdf.file)[7:], str(pdf.file)[7:])
+	os.system(comando)
+	contador_arquivos = subprocess.check_output("cd; cd projetos/slides/converter/templates/assets/img; ls | wc -l", stderr=subprocess.STDOUT, shell=True)
+	context = {'range': range(int(contador_arquivos))}
 	template = "home.html"
 	return render(request, template, context)
